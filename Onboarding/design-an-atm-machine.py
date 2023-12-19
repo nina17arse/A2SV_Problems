b5c903558ae7afa90e3e37ffdd9e9e462e343e76
Natnael Wondwoesn <70151258+nina17arse@users.noreply.github.com>
2023-12-19 17:31:28 +0300

    Added solution for Design an ATM Machine

---
+++ Onboarding/design-an-atm-machine.py
@@ -0,0 +1,36 @@
+class ATM:
+
+    def __init__(self):
+        self.banknotes_count = [0, 0, 0, 0, 0]
+        self.banknotes = [20, 50, 100, 200, 500]
+
+    def deposit(self, banknotesCount: List[int]) -> None:
+        for i in range(5):
+            self.banknotes_count[i] += banknotesCount[i]
+        # print(self.notes)
+        
+
+    def withdraw(self, amount: int) -> List[int]:
+        result = [0, 0, 0, 0, 0]
+
+        original_count = self.banknotes_count.copy()
+
+        for i in range(4, -1, -1):
+            result[i] = min(amount // self.banknotes[i], self.banknotes_count[i])
+            amount -= result[i] * self.banknotes[i]
+
+        if amount != 0:
+            return [-1]
+
+        for i in range(5):
+            self.banknotes_count[i] -= result[i]
+
+        return result
+        
+        
+
+
+# Your ATM object will be instantiated and called as such:
+# obj = ATM()
+# obj.deposit(banknotesCount)
+# param_2 = obj.withdraw(amount)
