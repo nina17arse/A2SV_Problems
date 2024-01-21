a65956d13684cd5944441d3633bc22eb348de467
Natnael Wondwoesn <70151258+nina17arse@users.noreply.github.com>
2024-01-21 22:45:48 +0300

    Added solution for Car Pooling

--- Algorithms/week2/car-pooling.py
+++ Algorithms/week2/car-pooling.py
@@ -1,13 +1,13 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         timeLine=defaultdict(int)
-        change=0
+        ch=0
         for passengers,start,end in trips:
             timeLine[start]+=passengers
             timeLine[end]-=passengers
         for i in sorted(timeLine.keys()):
-            change+=timeLine[i]
-            if change>capacity:
+            ch+=timeLine[i]
+            if ch>capacity:
                 return False
         return True
 
