e47b4981e9ba186c25c8de0cd66033f8912eee9b
Natnael Wondwoesn <70151258+nina17arse@users.noreply.github.com>
2024-02-29 14:38:44 +0300

    Add solution for Find Mode in Binary Search Tree

---
+++ Algorithms/linkedlist/leetcode/find-mode-in-binary-search-tree.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sol_rec(self,node,dic):
+        if node is None:
+            return None
+        if node:
+            dic[node.val]=dic.get(node.val,0) + 1
+
+        if node.left:
+            self.sol_rec(node.left,dic)
+        if node.right:
+            self.sol_rec(node.right,dic)
+
+        
+    
+        
+
+
+
+
+
+
+
+
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        ans=[]
+        dic={}
+        self.sol_rec(root,dic)
+        maxOccur=max(dic.values())
+        for k,v in dic.items():
+            if v==maxOccur:
+                ans.append(k)
+        return ans
+        
+        
+        
