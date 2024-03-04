7cf33271d5970b0e2a99ae5c1717ceb4000e8285
Natnael Wondwoesn <70151258+nina17arse@users.noreply.github.com>
2024-03-04 18:38:55 +0300

    Add solution for Fair Distribution of Cookies

--- Algorithms/Camp_1_part2/leetcode/fair-distribution-of-cookies.py
+++ Algorithms/Camp_1_part2/leetcode/fair-distribution-of-cookies.py
@@ -1,24 +1,25 @@
 class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
-        n = len(cookies)
-        scores = [0]*k
-        ans = [float("inf")]
-        zero_counts = k
-        print(scores)
-        def backtrack(index, maxes, zero_counts):
-            if zero_counts > n - index:
-                return
-            if index == n:
-                ans[0] = min(ans[0], max(maxes))
-                return
-            for i in range(k):
-                if not maxes[i]:
-                    zero_counts -= 1
-                maxes[i] += cookies[index]
-                backtrack(index+1, maxes, zero_counts)
-                maxes[i] -= cookies[index]
-                if not maxes[i]:
-                    zero_counts += 1
-        backtrack(0, scores, zero_counts)
-        return ans[0]
+        cur = [0]*k
+        n = len(cookies)        
+
+        def sol_rec(i, zero_count):
+            if n - i < zero_count:
+                return float('inf')
+            
+            if n == i:
+                return max(cur)
+            
+            answer = float('inf')
+            for j in range(k): 
+                zero_count -= int(cur[j] == 0)
+                cur[j] += cookies[i]
+                
+                answer = min(answer, sol_rec(i+1, zero_count))
+
+                cur[j] -= cookies[i]
+                zero_count += int(cur[j] == 0)
+
+            return answer
         
+        return sol_rec(0, k)
