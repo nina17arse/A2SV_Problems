23f3f9be8a76916bc0efccc8ab0ec0d298aa73b1
Natnael Wondwoesn <70151258+nina17arse@users.noreply.github.com>
2024-03-05 14:56:09 +0300

    Add solution for Combination Sum II

---
+++ Algorithms/Camp_1_part3/leetcode/combination-sum-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+  def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+    ans = []
+
+    def sol_rec(s, target, path):
+      if target < 0:
+        return
+      if target == 0:
+        ans.append(path.copy())
+        return
+
+      for i in range(s, len(candidates)):
+        if i > s and candidates[i] == candidates[i - 1]:
+          continue
+        path.append(candidates[i])
+        sol_rec(i + 1, target - candidates[i], path)
+        path.pop()
+
+    candidates.sort()
+    sol_rec(0, target, [])
+    return ans
+        
