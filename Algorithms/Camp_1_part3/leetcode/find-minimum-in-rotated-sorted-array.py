433ada79c535bbd1a706bec6b40e04749ae21ef9
Natnael Wondwoesn <70151258+nina17arse@users.noreply.github.com>
2024-03-06 18:42:49 +0300

    Add solution for Find Minimum in Rotated Sorted Array

--- Algorithms/Camp_1_part3/leetcode/find-minimum-in-rotated-sorted-array.py
+++ Algorithms/Camp_1_part3/leetcode/find-minimum-in-rotated-sorted-array.py
@@ -1,24 +1,10 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        firstele=nums[0]
-        lastele=nums[-1]
-        left,right=0,len(nums)-1
-        minN=float("inf")
-        if len(nums)==1:
-            return nums[0]
-        elif len(nums)==2:
-            return min(nums)
-        
-        while left<=right:
-            mid=(left+right)//2
-            if nums[mid]<minN:
-                minN=nums[mid]
-            if nums[mid] < nums[right]:
-                right=mid-1
-            else:
-                left=mid+1
-
-        return minN
+        nums.sort()
+        return nums[0]
+            
+            
+            
                 
 
         
