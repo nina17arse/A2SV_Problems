711cb0eebd34fed3ac01f75eba691b7c152e4b55
Natnael Wondwoesn <70151258+nina17arse@users.noreply.github.com>
2024-03-08 20:51:09 +0300

    Add solution for Reveal Cards In Increasing Order

---
+++ Algorithms/Camp_1_part3/leetcode/reveal-cards-in-increasing-order.py
@@ -0,0 +1,10 @@
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        deck.sort()
+        ans = [deck[-1]]
+        currIdx = len(deck)-2
+        while len(ans) != len(deck):
+            ans = [deck[currIdx]] + [ans[-1]] + ans[:-1]
+            currIdx -= 1
+        return ans
+        
