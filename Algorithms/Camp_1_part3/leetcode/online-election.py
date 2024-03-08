22d20c27741cd7fb79f39a8b5b161ae7a8c25285
Natnael Wondwoesn <70151258+nina17arse@users.noreply.github.com>
2024-03-08 17:35:14 +0300

    Add solution for Online Election

--- Algorithms/Camp_1_part3/leetcode/online-election.py
+++ Algorithms/Camp_1_part3/leetcode/online-election.py
@@ -1,19 +1,17 @@
 class TopVotedCandidate:
     def __init__(self, persons: List[int], times: List[int]):
-        self.time_winning =defaultdict()
+        self.tW =defaultdict()
         self.times = times                
-        # print(self.time_winning)
         vote_count = defaultdict(int)
         cur_max, cur_win = 0, -1
         for p, t in zip(persons, times):
             vote_count[p] += 1
-            # print(vote_count)
             if vote_count[p] >= cur_max: 
                 cur_max, cur_win = vote_count[p], p
-            self.time_winning[t] = cur_win
+            self.tW[t] = cur_win
             
         
         
 
     def q(self, t: int) -> int:
-        return self.time_winning[self.times[bisect_right(self.times, t)-1]]
+        return self.tW[self.times[bisect_right(self.times, t)-1]]
